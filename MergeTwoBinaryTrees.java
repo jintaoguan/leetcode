cb3189eb5f33b7f4e53ea1f1f8fb92b3a97a85bd
Jintao Guan <jintaoguan0817@gmail.com>
2020-05-19 00:58:38 -0700

    617. Merge Two Binary Trees

--- MergeTwoBinaryTrees.java
+++ MergeTwoBinaryTrees.java
@@ -1,16 +1,9 @@
-public class Solution {
+class Solution {
     public static void main(String[] argc) {
         Solution solution = new Solution();
         TreeNode t1 = new TreeNode(1);
         TreeNode t2 = new TreeNode(2);
         System.out.println(solution.mergeTrees(t1, t2));
-    }
-
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode(int x) { val = x; }
     }
 
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
@@ -21,9 +14,32 @@ public class Solution {
         } else if (t2 == null) {
             return t1;
         }
-        TreeNode root = new TreeNode(t1.val + t2.val);
+        TreeNode root = new TreeNode();
+        root.val = t1.val + t2.val;
         root.left = mergeTrees(t1.left, t2.left);
         root.right = mergeTrees(t1.right, t2.right);
         return root;
     }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
 }
+
