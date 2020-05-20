fac0bd2d3f7bd34836e1a6e5e323835f7e832353
Jintao Guan <jintaoguan0817@gmail.com>
2020-05-19 21:20:46 -0700

    100. Same Tree

--- SameTree.java
+++ SameTree.java
@@ -1,26 +1,40 @@
-public class Solution {
+class Solution {
     public static void main(String[] argc) {
         Solution solution = new Solution();
-        System.out.println(solution.isSameTree(null, null));
-    }
-
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode(int x) { val = x; }
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(1);
+        System.out.println(solution.isSameTree(t1, t2));
     }
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
             return true;
-        } else if (p != null && q != null) {
-            if (p.val != q.val) {
-                return false;
-            } else {
-                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-            }
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
-        return false;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }
+
