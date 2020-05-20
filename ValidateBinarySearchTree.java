906647272f1f1c31daddf719748cbc853c15a38a
Jintao Guan <jintaoguan0817@gmail.com>
2020-05-19 22:48:12 -0700

    98. Validate Binary Search Tree

--- ValidateBinarySearchTree.java
+++ ValidateBinarySearchTree.java
@@ -1,45 +1,44 @@
-public class Solution {
+class Solution {
     public static void main(String[] argc) {
         Solution solution = new Solution();
-        System.out.println(solution.isValidBST(null));
+        TreeNode root = new TreeNode(1);
+        System.out.println(solution.isValidBST(root));
     }
 
-    public static class TreeNode {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBSTHelper(root, null, null);
+    }
+
+    public boolean isValidBSTHelper(TreeNode root, TreeNode min, TreeNode max) {
+        if (root == null) {
+            return true;
+        }
+        if (max != null && root.val >= max.val) {
+            return false;
+        }
+        if (min != null && root.val <= min.val) {
+            return false;
+        }
+        return isValidBSTHelper(root.left, min, root) && isValidBSTHelper(root.right, root, max);
+    }
+
+    private static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode(int x) { val = x; }
-    }
 
-    // solution 1
-    public boolean isValidBST(TreeNode root) {
-        return isValidBSTHelper(root, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
+        TreeNode() {
+        }
 
-    public boolean isValidBSTHelper(TreeNode root, long min, long max) {
-        if (root == null) {
-            return true;
+        TreeNode(int val) {
+            this.val = val;
         }
-        if (root.val <= min || root.val >= max) {
-            return false;
-        }
-        return isValidBSTHelper(root.left, min, root.val) && isValidBSTHelper(root.right, root.val, max);
-    }
 
-
-    // solution 2
-    TreeNode prev = null;
-    public boolean isValidBST2(TreeNode root) {
-        if (root == null) {
-            return true;
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
         }
-        if (!isValidBST2(root.left)) {
-            return false;
-        }
-        if (prev != null && prev.val >= root.val) {
-            return false;
-        }
-        prev = root;
-        return isValidBST2(root.right);
     }
 }
+
